dcb96018a67b94246869c76fc12ee4fe330e0b4c
afnleaf <afnleafow@gmail.com>
2024-12-08 02:03:06 -0500

    fix lsp again

--- nvim/init.lua
+++ nvim/init.lua
@@ -207,7 +207,7 @@ require("lazy").setup({
             local lspconfig = require('lspconfig')
             local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-            -- New function to set up LSP keymaps properly
+            -- new function to set up LSP keymaps properly
             local function setup_lsp_keymaps(client, bufnr)
                 local function buf_set_keymap(mode, lhs, rhs, opts)
                     local options = {noremap = true, silent = true}
@@ -229,19 +229,6 @@ require("lazy").setup({
                 end
             end
 
-            vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
-                vim.lsp.handlers.hover, {
-                    border = "rounded",
-                    wrap_at = 100,
-                }
-            )
-
-            vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
-                vim.lsp.handlers.signature_help, {
-                    border = "rounded",
-                    wrap_at = 100,
-                }
-            )
 
             -- LSP setup function
             local function setup_lsp(server, config)
