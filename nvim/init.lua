f0e03e155b70fb5825edf720d6cfce52cb465c90
afnleaf <83483971+afnleaf@users.noreply.github.com>
2026-02-17 17:37:46 -0500

    Refactor init.lua with updated settings and plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,12 +6,13 @@ vim.g.maplocalleader = " "
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
+vim.opt.shada = ''
+vim.opt.colorcolumn = "80"
 
 -- Number line
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.signcolumn = "yes"
+vim.opt.signcolumn = "auto"
 
 -- Set up transparency
 vim.api.nvim_command('hi Normal guibg=NONE ctermbg=NONE')
@@ -56,24 +57,6 @@ vim.api.nvim_create_autocmd("FileChangedShellPost", {
 
 -- Reduce the time before CursorHold fires (milliseconds)
 vim.opt.updatetime = 300
--- Toggle LSP
-function Toggle_lsp()
-    local buf = vim.api.nvim_get_current_buf()
-    local clients = vim.lsp.get_clients({buffer = buf})
-    if #clients > 0 then
-        -- If there are active clients, stop them
-        for _, client in ipairs(clients) do
-            vim.lsp.stop_client(client.id)
-        end
-        vim.g.lsp_enabled = false
-        vim.notify("LSP disabled for current buffer")
-    else
-        -- Restart LSP for current buffer
-        vim.cmd("LspStart")
-        vim.g.lsp_enabled = true
-        vim.notify("LSP enabled for current buffer")
-    end
-end
 
 -- Toggle completion
 function Toggle_completion()
@@ -114,7 +97,12 @@ require("lazy").setup({
         event = { "BufReadPost", "BufNewFile" },
         config = function()
             require("nvim-treesitter.configs").setup({
-                ensure_installed = { "lua", "javascript", "typescript", "python" },
+                ensure_installed = {
+                    "lua",
+                    "javascript",
+                    "typescript",
+                    "python"
+                },
                 ignore_install = {},
                 modules = {},
                 sync_install = true,
@@ -129,10 +117,55 @@ require("lazy").setup({
             })
         end,
     },
+--    -- Solarized theme
+--    {
+--        'maxmx03/solarized.nvim',
+--        priority = 1000, -- Make sure to load this before other plugins
+--        opts = {
+--            -- Configure transparency
+--            transparent = {
+--                enabled = true,   -- Master switch for transparency
+--                normal = true,    -- Main editor background
+--                normalfloat = true, -- Floating windows
+--                telescope = true,
+--                nvimtree = true,
+--                -- You can add more components here if you like
+--            },
+--            -- You can optionally configure styles here
+--            styles = {
+--                comments = { italic = true },
+--                keywords = { italic = true },
+--                functions = { bold = true },
+--                strings = { italic = true },
+--            },
+--        },
+--        config = function(_, opts)
+--            -- CRITICAL: Set the background to 'light' for Solarized Light
+--            vim.o.background = 'light'
+--
+--            -- Enable true color support
+--            vim.o.termguicolors = true
+--
+--            -- Load the colorscheme
+--            require('solarized').setup(opts)
+--            vim.cmd.colorscheme 'solarized'
+--
+--            -- Your custom highlight for Visual mode (optional, you can remove this)
+--            vim.api.nvim_set_hl(0, 'Visual', { bg = "#93a1a1" })
+--
+--            -- You may not need these force-transparency lines anymore,
+--            -- as the theme's `transparent = true` option should handle it.
+--            -- You can try commenting them out to see if it still works.
+--            -- vim.api.nvim_command('hi Normal guibg=NONE ctermbg=NONE')
+--            -- vim.api.nvim_command('hi LineNr guibg=NONE ctermbg=NONE')
+--            -- ... etc
+--        end,
+--    },
     -- Monokai theme
     {
         "tanvirtin/monokai.nvim",
-        priority = 1000, -- Load colorscheme before other plugins
+        -- Load colorscheme before other plugins
+        priority = 1000,
         config = function()
             -- Configure the colorscheme
             require('monokai').setup {
@@ -164,33 +197,6 @@ require("lazy").setup({
             vim.api.nvim_command('hi EndOfBuffer guibg=NONE ctermbg=NONE')
         end,
     },
-    -- Minimap
-    --{
-    --   'gorbit99/codewindow.nvim',
-    --    dependencies = {
-    --        "nvim-treesitter/nvim-treesitter",
-    --    },
-    --    config = function()
-    --        local codewindow = require('codewindow')
-    --        codewindow.setup({
-    --            active_in_terminals = false,
-    --            max_minimap_height = 100,
-    --            minimap_width = 20,
-    --            use_treesitter = true,
-    --            exclude_filetypes = {'help'},
-    --            window_border = 'single',
-    --            use_lsp = true,
-    --            screen_bounds = 'lines',
-    --            z_index = 1,
-    --            window = {
-    --                scrollbar = true,
-    --                focusable = true,
-    --            },
-    --        })
-
-    --        codewindow.apply_default_keybinds()
-    --    end,
-    --},
     -- Auto-pairs
     {
         'windwp/nvim-autopairs',
@@ -198,12 +204,18 @@ require("lazy").setup({
         config = function()
             local npairs = require('nvim-autopairs')
             npairs.setup({
-                check_ts = true, -- Use treesitter to check for pairs
-                enable_check_bracket_line = true,  -- Don't add pairs if it already has a close pair in the same line
-                ignored_next_char = "[%w%.]", -- Don't add pairs if the next char is alphanumeric
+                -- Use treesitter to check for pairs
+                check_ts = true,
+                -- Don't add pairs if it already has
+                -- a close pair in the same line
+                enable_check_bracket_line = true,
+                -- Don't add pairs if the next char is alphanumeric
+                ignored_next_char = "[%w%.]",
                 -- Add spaces between parentheses
-                enable_afterquote = true,  -- add bracket pairs after quote
-                enable_moveright = true,   -- enable moving right when inserting pairs
+                -- add bracket pairs after quote
+                enable_afterquote = true,
+                -- enable moving right when inserting pairs
+                enable_moveright = true,
             })
 
             -- Add specific rules
@@ -227,7 +239,15 @@ require("lazy").setup({
                 Rule('<', '>')
                     :with_pair(function()
                         -- Add pair only in specific file types
-                        return vim.tbl_contains({ 'html', 'xml', 'tsx', 'jsx', 'vue', 'svelte', 'rs' }, vim.bo.filetype)
+                        return vim.tbl_contains({
+                            'html',
+                            'xml',
+                            'tsx',
+                            'jsx',
+                            'vue',
+                            'svelte',
+                            'rust'
+                        }, vim.bo.filetype)
                     end)
             })
             npairs.add_rules({
@@ -241,168 +261,6 @@ require("lazy").setup({
             })
         end
     },
-    -- Mason for LSP management
-    {
-        "williamboman/mason.nvim",
-        lazy = true,
-        event = "VeryLazy",
-        build = ":MasonUpdate",
-        config = function()
-            require("mason").setup()
-        end
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = {
-                    "lua_ls",       -- Lua
-                    "pyright",      -- Python
-                    "cssls",        -- CSS
-                    "html",         -- HTML
-                    "emmet_ls",     -- Emmet
-                    "jsonls",       -- JSON
-                    "ts_ls",        -- Javascript/Typescript
-                    -- i don't want to use one for rust, compiler better
-
-                },
-                automatic_installation = true,
-            })
-        end
-    },
-    -- LSP Configuration
-    {
-        "neovim/nvim-lspconfig",
-        event = { "BufReadPre", "BufNewFile"},
-        dependencies = {
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/nvim-cmp",
-            "L3MON4D3/LuaSnip",
-            "b0o/schemastore.nvim",
-        },
-        config = function()
-            local lspconfig = require('lspconfig')
-            local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
-            -- new function to set up LSP keymaps properly
-            local function setup_lsp_keymaps(client, bufnr)
-                local function buf_set_keymap(mode, lhs, rhs, opts)
-                    local options = {noremap = true, silent = true}
-                    if opts then options = vim.tbl_extend('force', options, opts) end
-                    vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, options)
-                end
-
-                if client.server_capabilities then
-                    -- Set up all your existing keymaps as buffer-local
-                    buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
-                    buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
-                    buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>')
-                    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
-                    buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>')
-                    buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>')
-                    buf_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>')
-                    buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>')
-                    buf_set_keymap('n', '<leader>f', '<cmd>lua vim.lsp.buf.format()<CR>')
-                end
-            end
-
-
-            -- LSP setup function
-            local function setup_lsp(server, config)
-                config = config or {}
-                config.capabilities = capabilities
-                config.on_attach = function(client, bufnr)
-                    setup_lsp_keymaps(client, bufnr)
-                end
-                lspconfig[server].setup(config)
-            end
-
-            local function get_mason_bin(bin)
-                local is_windows = vim.fn.has("win32") == 1
-                local mason_root = vim.fs.normalize(vim.fn.stdpath("data") .. "/mason/bin")
-
-                if is_windows then
-                    return mason_root .. bin .. ".cmd"
-                else
-                    return mason_root .. bin
-                end
-            end
-
-            -- Set up each LSP server with all your existing configurations
-            setup_lsp('lua_ls', {
-                cmd = { get_mason_bin("/lua-language-server") },
-                settings = {
-                    Lua = {
-                        diagnostics = {
-                            globals = { 'vim' }
-                        },
-                        workspace = {
-                            library = vim.api.nvim_get_runtime_file("", true),
-                            checkThirdParty = false,
-                        },
-                        telemetry = {
-                            enable = false,
-                        },
-                    },
-                },
-            })
-
-            setup_lsp('pyright', {
-                settings = {
-                    python = {
-                        analysis = {
-                            typeCheckingMode = "basic",
-                            autoSearchPaths = true,
-                            useLibraryCodeForTypes = true,
-                        },
-                    },
-                },
-            })
-
-            setup_lsp('html', {
-                filetypes = { "html", "htmldjango" },
-            })
-
-            setup_lsp('cssls')
-
-            setup_lsp('jsonls', {
-                settings = {
-                    json = {
-                        schemas = require('schemastore').json.schemas(),
-                        validate = { enable = true },
-                    },
-                },
-            })
-
-            setup_lsp('emmet_ls', {
-                cmd = { get_mason_bin("/emmet-ls"), "--stdio" },
-                filetypes = {
-                    'html', 'css', 'scss', 'javascript',
-                    'javascriptreact', 'typescriptreact',
-                    'svelte', 'vue', 'htmldjango'
-                },
-            })
-
-            setup_lsp("ts_ls", {
-                cmd = { get_mason_bin("/typescript-language-server"), "--stdio"},
-                filetypes = {
-                    "javascript", "javascriptreact", "javascript.jsx",
-                    "typescript", "typescriptreact", "typescript.tsx"
-                },
-                settings = {
-                    javascript = {
-                        suggesstFromUseImport = true,
-                        importModuleSpecifier = "relative"
-                    },
-                    typescript = {
-                        suggesstFromUseImport = true,
-                        importModuleSpecifier = "relative"
-                    },
-                }
-            })
-
-        end
-    },
     -- Autocompletion setup
     {
         'hrsh7th/nvim-cmp',
@@ -412,7 +270,7 @@ require("lazy").setup({
             'saadparwaiz1/cmp_luasnip',
             'hrsh7th/cmp-nvim-lsp',
             'hrsh7th/cmp-buffer',
-            'hrsh7th/cmp-path',
+            --'hrsh7th/cmp-path',
             'hrsh7th/cmp-cmdline',
         },
         config = function()
@@ -445,7 +303,7 @@ require("lazy").setup({
                     { name = 'nvim_lsp' },
                     { name = 'luasnip' },
                     { name = 'buffer' },
-                    { name = 'path' },
+                    --{ name = 'path' },
                 }),
             })
         end
@@ -453,19 +311,12 @@ require("lazy").setup({
 })
 
 -- Keymaps
-vim.api.nvim_set_keymap('n', '<leader>tl', ':lua Toggle_lsp()<CR>',
-    {noremap = true, silent = true, desc = 'Toggle LSP'})
+-- toggle auto completion
 vim.api.nvim_set_keymap('n', '<leader>tc', ':lua Toggle_completion()<CR>',
     {noremap = true, silent = true, desc = 'Toggle Completion'})
+-- toggle relative number
 vim.api.nvim_set_keymap('n', '<leader>tr', ':set relativenumber!<CR>',
     {noremap = true, silent = true, desc = 'Toggle relative line numbers'})
-
-
-
--- Keymaps for minimap control
---vim.api.nvim_set_keymap('n', '<Leader>mm', ':lua require("codewindow").toggle_minimap()<CR>',
---    {noremap = true, silent = true})
---vim.api.nvim_set_keymap('n', '<Leader>mo', ':lua require("codewindow").open_minimap()<CR>',
---    {noremap = true, silent = true})
---vim.api.nvim_set_keymap('n', '<Leader>mc', ':lua require("codewindow").close_minimap()<CR>',
---    {noremap = true, silent = true})
+-- toggle number line
+vim.api.nvim_set_keymap('n', '<leader>tn', ':set number!<CR>',
+    {noremap = true, silent = true, desc = 'Toggle line numbers'})
